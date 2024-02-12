5f9b17f2397718db367194fc74032a6710788485
zzVIPz <43119686+zzVIPz@users.noreply.github.com>
2024-02-11 19:12:34 -0500

    set test user data

---
+++ src/data/users.ts
@@ -0,0 +1,12 @@
+import { User } from '../types/generalTypes';
+
+const users: User[] = [
+  {
+    id: 'test-id',
+    username: 'test-username',
+    age: 'test-age',
+    hobbies: ['test-hobby'],
+  },
+];
+
+export default users;
