205d82f604d1139976b0a8b04371bb1d985b34fe
zzVIPz <43119686+zzVIPz@users.noreply.github.com>
2024-02-11 22:41:49 -0500

    implement user id validation

--- src/api/getUserById.ts
+++ src/api/getUserById.ts
@@ -3,15 +3,21 @@ import users from '../data/users';
 import { HTTP_RESPONSE_CODES } from '../types/generalTypes';
 import { onResponseSend } from '../utils/onResponseSend';
 import parseUrl from '../utils/parseUrl';
+import { validate } from 'uuid';
 
 const getUserById = (req: IncomingMessage, res: ServerResponse) => {
-  const { userId } = parseUrl(req.url);
-  const user = users.find((user) => user.id === userId);
+  const { userId = '' } = parseUrl(req.url);
 
-  if (user) {
-    onResponseSend(res, HTTP_RESPONSE_CODES.OK, user);
+  if (!validate(userId)) {
+    onResponseSend(res, HTTP_RESPONSE_CODES.BAD_REQUEST, `Used id ${userId} is not valid`);
   } else {
-    onResponseSend(res, HTTP_RESPONSE_CODES.NOT_FOUND, `User with id ${userId} does not found`);
+    const user = users.find((user) => user.id === userId);
+
+    if (user) {
+      onResponseSend(res, HTTP_RESPONSE_CODES.OK, user);
+    } else {
+      onResponseSend(res, HTTP_RESPONSE_CODES.NOT_FOUND, `User with id ${userId} does not found`);
+    }
   }
 };
 
